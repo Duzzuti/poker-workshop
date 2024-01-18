748951fa05c6fc26134f22447e74bdd547bf013c
Duzzuti <duzzuti@gmail.com>
2024-01-18 18:45:05 +0000

    Add game and deck cpp docs

--- src/deck.cpp
+++ src/deck.cpp
@@ -3,28 +3,34 @@
 #include "logger.h"
 
 const char* Card::toString() const {
-    static char str[CARD_STR_LEN];  // Increased size to accommodate null terminator and Unicode symbol
+    // Size should accommodate null terminator and Unicode symbol
+    static char str[CARD_STR_LEN];
 
+    // Invalid suit
     if (this->suit > 3) {
         PLOG_FATAL << "Invalid suit: " << this->suit;
         throw std::logic_error("Invalid suit");
     }
+    // Add the suit symbol to the string (unicode)
     std::strncpy(str, SUIT_SYMBOLS[this->suit], 3);
 
+    // Invalid rank
     if (this->rank < 2 || this->rank > 14) {
         PLOG_FATAL << "Invalid rank: " << this->rank;
         throw std::logic_error("Invalid rank");
     }
 
+    // Add the rank to the string
     str[3] = CARD_RANKS[this->rank - 2];
 
-    str[4] = '\0';  // Null-terminate the string
+    // Null-terminate the string
+    str[4] = '\0';
 
     return str;
 }
 
 Deck::Deck() noexcept {
-    // generate poker deck
+    // generate a 53 cards poker deck
     u_int8_t i = 0;
     for (u_int8_t suit = 0; suit < 4; suit++) {
         for (u_int8_t rank = 2; rank < 15; rank++) {
@@ -42,6 +48,7 @@ void Deck::shuffle() noexcept {
 
 Card Deck::draw() {
     // draw card from deck
+    // if deck is empty throw exception
     if (this->len == 0) {
         PLOG_FATAL << "Deck is empty";
         throw std::logic_error("Deck is empty");
@@ -51,7 +58,7 @@ Card Deck::draw() {
 }
 
 const char* Deck::toString(const char sep) const {
-    // print deck
+    // print deck by concatenating all cards in deck with separator
     static char str[CARD_NUM * (CARD_STR_LEN + 1)];
     for (u_int8_t i = 0; i < this->len; i++) {
         std::strncat(str, this->cards[i].toString(), CARD_STR_LEN);
@@ -62,10 +69,12 @@ const char* Deck::toString(const char sep) const {
 
 Card Deck::getRandomCardExcept(const Card cards[], const u_int8_t cardsLen, const int8_t suit, const u_int8_t ranks[], const u_int8_t rankLen) noexcept {
     // get random card from deck except cards in array
-    // or with suit if suit != -1
-    // or with ranks if ranks.size() > 0
+    // except with suit if suit != -1
+    // except with ranks if ranks.size() > 0
     while (true) {
+        // gets random card
         Card card{.rank = (u_int8_t)((std::rand() % 13) + 2), .suit = (u_int8_t)(std::rand() % 4)};
+        // check conditions
         if ((suit == -1 || card.suit != suit) && (!rankLen || std::find(ranks, &ranks[rankLen], card.rank) == &ranks[rankLen]) && std::find(cards, &cards[cardsLen], card) == &cards[cardsLen]) {
             return card;
         }
@@ -74,7 +83,7 @@ Card Deck::getRandomCardExcept(const Card cards[], const u_int8_t cardsLen, cons
 
 Card Deck::getRandomCardExceptCardsWith(const Card exceptionCards[], const u_int8_t cardsLen, const int8_t suit, const int8_t rank) noexcept {
     // get random card from deck with suit if suit != -1 and rank if rank != -1
-
+    // except cards in array
     Card card;
     do {
         if (suit == -1 && rank == -1)  // get random card
@@ -85,6 +94,7 @@ Card Deck::getRandomCardExceptCardsWith(const Card exceptionCards[], const u_int
             card = Card{.rank = (u_int8_t)((std::rand() % 13) + 2), .suit = (u_int8_t)suit};
         else  // get card with suit and rank
             card = Card{.rank = (u_int8_t)rank, .suit = (u_int8_t)suit};
+        // check if card is in exceptionCards
     } while (std::find(exceptionCards, &exceptionCards[cardsLen], card) != &exceptionCards[cardsLen]);
     return card;
 }
