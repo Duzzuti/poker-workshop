b621bd1757e06b55a039b83f1300fdf4da19e558
Duzzuti <duzzuti@gmail.com>
2024-01-30 19:06:26 +0000

    Add hand strengths cpp docs

--- src/hand_strengths.cpp
+++ src/hand_strengths.cpp
@@ -5,14 +5,17 @@
 void HandStrengths::getHandStrengths(const std::unique_ptr<Player> players[], const Data& data, HandStrengths result[]) noexcept {
     for (u_int8_t i = 0; i < data.numPlayers; i++) {
         if (!(data.roundData.playerFolded[i] || data.gameData.playerOut[i])) {
+            // only calculate hand strength if player is still in the game
             result[i] = getHandStrength(players[i]->getHand(), data.roundData.communityCards);
         }
     }
 }
 
 HandStrengths HandStrengths::getHandStrength(const std::pair<Card, Card>& hand, const Card community[]) noexcept {
+    // saves the number of cards of each suit and rank
     u_int8_t suits[4] = {0, 0, 0, 0};
     u_int8_t ranks[13] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    // add cards to suits and ranks
     suits[hand.first.suit]++;
     suits[hand.second.suit]++;
     for (u_int8_t j = 0; j < 5; j++) {
@@ -24,56 +27,64 @@ HandStrengths HandStrengths::getHandStrength(const std::pair<Card, Card>& hand, 
         ranks[community[j].rank - 2]++;
     }
 
-    u_int8_t rank11 = 0;
-    u_int8_t rank12 = 0;
-    u_int8_t rank13 = 0;
-    u_int8_t rank14 = 0;
-    u_int8_t rank15 = 0;
-    u_int8_t rank21 = 0;
-    u_int8_t rank22 = 0;
-    u_int8_t rank23 = 0;
-    u_int8_t rank31 = 0;
-    u_int8_t rank32 = 0;
-    u_int8_t rank4 = 0;
-    int8_t flush_suit = -1;
+    // saves the ranks for the different kinds
+    // 0 means not found, else rank (2-14)
+    u_int8_t rank11 = 0;    // rank of highest card
+    u_int8_t rank12 = 0;    // rank of second highest card
+    u_int8_t rank13 = 0;    // rank of third highest card
+    u_int8_t rank14 = 0;    // rank of fourth highest card
+    u_int8_t rank15 = 0;    // rank of fifth highest card
+    u_int8_t rank21 = 0;    // rank of highest pair
+    u_int8_t rank22 = 0;    // rank of second highest pair
+    u_int8_t rank23 = 0;    // rank of third highest pair
+    u_int8_t rank31 = 0;    // rank of highest three of a kind
+    u_int8_t rank32 = 0;    // rank of second highest three of a kind
+    u_int8_t rank4 = 0;     // rank of four of a kind
+
+    int8_t flush_suit = -1; // -1 (no flush) or suit of flush (0-3)
 
     for (u_int8_t j = 0; j < 4; j++) {
         if (suits[j] >= 5) {
-            // flush
+            // flush found with suit j
             flush_suit = j;
             break;
         }
     }
 
+    // find pairs, three of a kind and four of a kind from highest to lowest rank
     for (int8_t j = 12; j >= 0; j--) {
         switch (ranks[j]) {
             case 4:
+                // four of a kind (highest)
                 rank4 = j + 2;
                 break;
             case 3:
-                if (rank31 == 0)
+                // three of a kind
+                if (rank31 == 0)        // first three of a kind (highest)
                     rank31 = j + 2;
-                else if (rank32 == 0)
+                else                    // second three of a kind (second highest)
                     rank32 = j + 2;
                 break;
             case 2:
-                if (rank21 == 0)
+                // pair
+                if (rank21 == 0)        // first pair (highest)
                     rank21 = j + 2;
-                else if (rank22 == 0)
+                else if (rank22 == 0)   // second pair (second highest)
                     rank22 = j + 2;
-                else if (rank23 == 0)
+                else                    // third pair (third highest)
                     rank23 = j + 2;
                 break;
             case 1:
-                if (rank11 == 0)
+                // single card
+                if (rank11 == 0)        // first single card (highest)
                     rank11 = j + 2;
-                else if (rank12 == 0)
+                else if (rank12 == 0)   // second single card (second highest)
                     rank12 = j + 2;
-                else if (rank13 == 0)
+                else if (rank13 == 0)   // third single card (third highest)
                     rank13 = j + 2;
-                else if (rank14 == 0)
+                else if (rank14 == 0)   // fourth single card (fourth highest)
                     rank14 = j + 2;
-                else if (rank15 == 0)
+                else if (rank15 == 0)   // fifth single card (fifth highest)
                     rank15 = j + 2;
                 break;
             default:
@@ -81,25 +92,40 @@ HandStrengths HandStrengths::getHandStrength(const std::pair<Card, Card>& hand, 
         }
     }
 
-    // available hands: high card, pair, two pair, three of a kind, straight, flush, full house, four of a kind, straight flush, royal flush
+    // possible hands: high card, pair, two pair, three of a kind, straight, flush, full house, four of a kind, straight flush, royal flush
 
     if (flush_suit != -1) {
         // flush
-        // available hands: flush, full house, four of a kind, straight flush, royal flush
+        // possible hands: flush, full house, four of a kind, straight flush, royal flush
+        if (rank4 != 0) {
+            // four of a kind, cannot be a straight flush or royal flush (because 4 cards of the same rank are needed for four of a kind)
+            return HandStrengths{HandKinds::FOUR_OF_A_KIND, (u_int32_t)(rank4 << 4) | std::max<u_int8_t>(std::max<u_int8_t>(rank11, rank21), rank31)};
+        } else if (rank31 != 0 && (rank32 != 0 || rank21 != 0)) {
+            // full house, cannot be a straight flush or royal flush (because 5 cards of two ranks are needed for a full house)
+            return HandStrengths{HandKinds::FULL_HOUSE, (u_int32_t)(rank31 << 4) | std::max<u_int8_t>(rank32, rank21)};
+        }
+        // possible hands: flush, straight flush, royal flush
+        
+        // cards that are part of the flush
         Card flush_cards[7];
+        // number of cards that are part of the flush
         u_int8_t flush_length = 0;
 
+        // check hand cards
         if (hand.first.suit == flush_suit) flush_cards[flush_length++] = hand.first;
         if (hand.second.suit == flush_suit) flush_cards[flush_length++] = hand.second;
 
+        // check community cards
         for (u_int8_t j = 0; j < 5; j++) {
             if (community[j].suit == flush_suit) flush_cards[flush_length++] = community[j];
         }
+        // sort flush cards from highest to lowest rank
         std::sort(flush_cards, &flush_cards[flush_length], [](const Card& card1, const Card& card2) { return card1.rank > card2.rank; });
         for (u_int8_t j = 0; j < flush_length - 4; j++) {
+            // check if straight flush
             if (flush_cards[j].rank == flush_cards[j + 4].rank + 4) {
                 if (flush_cards[j].rank == 14) {
-                    // royal flush
+                    // royal flush, because straight flush with ace as highest card
                     return HandStrengths{HandKinds::ROYAL_FLUSH, 0};
                 } else {
                     // straight flush
@@ -107,28 +133,19 @@ HandStrengths HandStrengths::getHandStrength(const std::pair<Card, Card>& hand, 
                 }
             }
         }
+        // flush
+        return HandStrengths{HandKinds::FLUSH, getRankStrength(flush_cards, 5)};
+    } else {
+        // no flush
+        // possible hands: high card, pair, two pair, three of a kind, straight, full house, four of a kind
         if (rank4 != 0) {
             // four of a kind
             return HandStrengths{HandKinds::FOUR_OF_A_KIND, (u_int32_t)(rank4 << 4) | std::max<u_int8_t>(std::max<u_int8_t>(rank11, rank21), rank31)};
         } else if (rank31 != 0 && (rank32 != 0 || rank21 != 0)) {
             // full house
             return HandStrengths{HandKinds::FULL_HOUSE, (u_int32_t)(rank31 << 4) | std::max<u_int8_t>(rank32, rank21)};
-        } else {
-            // flush
-            return HandStrengths{HandKinds::FLUSH, getRankStrength(flush_cards, 5)};
         }
-    } else {
-        // no flush
-        // available hands: high card, pair, two pair, three of a kind, straight, full house, four of a kind
-        if (rank4 != 0) {
-            // four of a kind
-            return HandStrengths{HandKinds::FOUR_OF_A_KIND, (u_int32_t)(rank4 << 4) | std::max<u_int8_t>(std::max<u_int8_t>(rank11, rank21), rank31)};
-        }
-        if (rank31 != 0 && (rank32 != 0 || rank21 != 0)) {
-            // full house
-            return HandStrengths{HandKinds::FULL_HOUSE, (u_int32_t)(rank31 << 4) | std::max<u_int8_t>(rank32, rank21)};
-        }
-        // available hands: high card, pair, two pair, three of a kind, straight
+        // possible hands: high card, pair, two pair, three of a kind, straight
         for (u_int8_t j = 12; j >= 4; j--) {
             if (ranks[j] != 0 && ranks[j - 1] != 0 && ranks[j - 2] != 0 && ranks[j - 3] != 0 && ranks[j - 4] != 0) {
                 // straight
@@ -144,25 +161,27 @@ HandStrengths HandStrengths::getHandStrength(const std::pair<Card, Card>& hand, 
         } else if (rank21 != 0) {
             // pair
             return HandStrengths{HandKinds::PAIR, (u_int32_t)(rank21 << 12) | (u_int32_t)(rank11 << 8) | (u_int32_t)(rank12 << 4) | rank13};
-        } else {
-            // high card
-            return HandStrengths{HandKinds::HIGH_CARD, (u_int32_t)(rank11 << 16) | (u_int32_t)(rank12 << 12) | (u_int32_t)(rank13 << 8) | (u_int32_t)(rank14 << 4) | rank15};
         }
+        // high card
+        return HandStrengths{HandKinds::HIGH_CARD, (u_int32_t)(rank11 << 16) | (u_int32_t)(rank12 << 12) | (u_int32_t)(rank13 << 8) | (u_int32_t)(rank14 << 4) | rank15};
     }
 }
 
 u_int32_t HandStrengths::getRankStrength(const Card sortedCards[], const u_int8_t num) noexcept {
     u_int32_t rankStrength = 0;
     for (u_int8_t i = 0; i < num; i++) {
+        // shift the rank of the card to the left by 4 * (num-1, num-2, ..., 0) bits and add it to rankStrength
         rankStrength |= sortedCards[i].rank << 4 * (num - i - 1);
     }
     return rankStrength;
 }
 
 bool operator>(const HandStrengths& lhs, const HandStrengths& rhs) noexcept {
+    // compare hand kinds
     if (lhs.handkind > rhs.handkind) {
         return true;
     } else if (lhs.handkind == rhs.handkind) {
+        // compare rank strengths
         return lhs.rankStrength > rhs.rankStrength;
     }
     return false;
